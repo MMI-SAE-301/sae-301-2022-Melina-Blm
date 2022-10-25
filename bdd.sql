ae6f3ca64419967c5e5c0411b3a6595bf938a5d6
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-25 13:30:47 +0200

    ajout création bdd

--- bdd.sql
+++ bdd.sql
@@ -1,7 +1,22 @@
 --
 -- code pour la création des tables
 --
+-- La table user sera ajouté directement sur supabase
+CREATE TABLE 'MONTRE' (
+"id_montre" int NOT NULL,
+"bracelet" varchar,
+"ecran" varchar,
+"boitier" varchar,
+PRIMARY KEY (id_montre),
+FOREIGN KEY (id_materiel) REFERENCES MATERIEL ("id_materiel"),
+);
 
+CREATE TABLE 'MATERIEL' (
+"id_materiel" int NOT NULL,
+"libelle_materiel" varchar,
+PRIMARY KEY (id_materiel)
+
+);
 
 
 
