82c8343712c62ff57c2f00320b5bbaa90fa647e1
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-26 08:21:31 +0200

    debut du front du sign in

--- src/pages/signin.vue
+++ src/pages/signin.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "@vue/reactivity";
 import { supabase, user } from "../supabase";
-import { createClient } from '@supabase/supabase-js'
+
 
 async function signIn(data, node) {
     const { user, error } = await (nvlUtilisateur.value
@@ -13,27 +13,37 @@ async function signIn(data, node) {
     }
 }
 const nvlUtilisateur = ref(false);
+
+
 </script>
 <template>
-    <div>
-        <button v-if="user" @pointerdown="supabase.auth.signOut()">
-            Se déconnecter ({{ user.email }})
-        </button>
-        <FormKit v-else type="form" :submit-label="nvlUtilisateur ? 'S\'inscrire' : 'Se connecter'" @submit="signIn">
-            <FormKit name="email" label="Votre eMail" type="email" />
-            <FormKit name="password" label="Mot de passe" type="password" />
-            <formKit label="Nouvel utilisateur ?" name="nvlUtilisateur" type="checkbox" v-model="nvlUtilisateur" />
-        </FormKit>
+
+    <h1 class="text-50s font-reemkufi font-semibold tracking-widest text-dark-black text-center mt-12">TIK TAK</h1>
+    <div class="bg-bg-grey">
+        <h1 class="text-50s font-reemkufi font-semibold tracking-widest text-dark-black text-center">WATCHES FOR ALL
+        </h1>
+
+
         <div>
+            <button v-if="user" @pointerdown="supabase.auth.signOut()">
+                Se déconnecter ({{ user.email }})
+            </button>
+            <FormKit v-else type="form" :submit-label="nvlUtilisateur ? 'S\'inscrire' : 'Se connecter'"
+                @submit="signIn">
+                <FormKit name="email" label="Votre eMail" type="email" />
+                <FormKit name="password" label="Mot de passe" type="password" />
+                <formKit label="Nouvel utilisateur ?" name="nvlUtilisateur" type="checkbox" v-model="nvlUtilisateur" />
+            </FormKit>
             <div>
                 <button v-if="user" @pointerdown="supabase.auth.signOut()">
                     Se déconnecter ({{ user.email }})
                 </button>
-                <button v-else @pointerdown="supabase.auth.signIn({ provider: 'github' })">
-                    Se connecter avec Github
-                </button>
+
             </div>
         </div>
+        
+
+
     </div>
 
-</template> 
+</template>
