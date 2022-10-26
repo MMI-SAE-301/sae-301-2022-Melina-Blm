f27935b97deeac3f20fafb50892924f56cad6d89
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-26 15:40:42 +0200

    création de la page id

---
+++ src/pages/montres/edit/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import FormsMontre from "../../../components/FormsMontre.vue";
+
+defineProps<{
+    id: string;
+}>();
+</script>
+<template>
+    <h1>Vous avez commandé ! </h1>
+    <FormsMontre :id="id" />
+    <router-link to="/">Retourner à l'accueil</router-link>
+
+</template>
