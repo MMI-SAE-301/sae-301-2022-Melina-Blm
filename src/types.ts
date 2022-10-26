048b37288e209b7ec8536197e07064b797cd2711
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-26 16:57:53 +0200

    ajout const materiaux

--- src/types.ts
+++ src/types.ts
@@ -10,4 +10,23 @@ export const colors = {
     "#678FA6": "Bleu",
     "#8E77A1": "Violet",
     "#C16E5C": "Orange",
+    "#9E3939": "Rouge",
+    "#606060": "Gris",
+    "#333687": "Bleu fonçé",
 };
+
+export const materiaux = [
+    {
+        value: "https://www.navailles.fr/wp-content/uploads/2021/11/Cuir_Marron.png",
+        label: "Cuir brun",
+    },
+    {
+        value: "https://previews.123rf.com/images/prapann/prapann1709/prapann170900389/85775110-texture-en-cuir-dor%C3%A9-en-crocodile-pour-le-fond.jpg",
+        label: "Cuir doré",
+    },
+    {
+        value: "https://www.tissus-et-tendance.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/n/en3100-bleu-simili-cuir-ameublement-faux-leather-tissus-et-tendance-fr-ignifuge-fr-banquette-tornado-grain.jpg",
+        label: "Cuir bleu",
+    },
+
+];
