3f6925b48cec4b272967eb38afb35855f90b659a
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-26 16:58:59 +0200

    création du formulaire pour les matériaux

---
+++ src/components/FormMateriaux.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { materiaux } from '@/types'
+
+
+defineProps<{
+    name?: string;
+    label?: string;
+}>();
+
+</script>
+<template>
+    <FormKit type="form">
+        <FormKit :name="name" :label="label" value="#FFFFFF" type="radio" :options="materiaux" :sections-schema="{
+            inner: { $el: null },
+            decorator: { $el: null },
+        }" input-class="peer sr-only" options-class="flex gap-4 my-3">
+            <template #label="context">
+                <div class="h-9 w-9 rounded-full border-4 peer-checked:border-blue-500"
+                    :style="{ backgroundImage: `url('${context.option.value}')` }">
+                </div>
+                <span class="sr-only">{{ context.option.label }}</span>
+            </template>
+        </FormKit>
+    </FormKit>
+</template>
+
+
