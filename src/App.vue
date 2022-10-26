8afe70d9dfc44c75c1fe05d394c5833cbac68cac
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-26 09:55:33 +0200

    import du footer dans l'app vue

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,17 @@
 
   </nav>
 
+
   <!-- Affiche les pages -->
   <Suspense>
     <router-view class="m-2 border-2 p-2" />
   </Suspense>
+  <FooterTikTakVue />
 </template>
 
 <script setup lang="ts">
 
 import MenuTikTak from './components/Menu.vue'
+import FooterTikTakVue from './components/FooterTikTak.vue';
 
 </script>
