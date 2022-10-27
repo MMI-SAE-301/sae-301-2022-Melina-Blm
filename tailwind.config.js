09dc8c6b4603d461f1d6bda790e6e12e7d0331e5
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-27 08:36:10 +0200

    ajout du dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@ const formKitTailwind = require("@formkit/themes/tailwindcss");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+
       fontFamily: {
         'raleway': ['Raleway', 'sans-serif'],
         'reemkufi': ['Reem Kufi'],
