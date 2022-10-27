358b61565c36bd2d67e3a61f8f73028be807ad83
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-27 09:01:20 +0200

    usage du dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <nav>
-    <MenuTikTak />
-  </nav>
+  <div :class="isDark ? 'dark' : ''">
+    <nav>
+      <MenuTikTak />
 
-  <!-- Affiche les pages -->
-  <Suspense>
-    <router-view class="m-2 border-2 p-2" />
-  </Suspense>
-  <FooterTikTakVue />
+      <!--BOUTTON DARK MODE A ACTIVER SUR LA PAGE ABOUT ET COLLECTIONS
+      <div>
+        <button checked @click="isDark = !isDark">Dark Mode</button>
+
+      </div>
+      -->
+    </nav>
+
+
+
+    <!-- Affiche les pages -->
+    <Suspense>
+      <router-view class="m-2 border-2 p-2" />
+    </Suspense>
+
+
+    <FooterTikTakVue />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -16,3 +29,88 @@ import MenuTikTak from './components/Menu.vue'
 import FooterTikTakVue from './components/FooterTikTak.vue';
 
 </script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      isDark: false,
+    };
+  },
+};
+</script>
+<style>
+.switch {
+  width: 60px;
+  height: 34px;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.1);
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 23px;
+  width: 23px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked+.slider {
+  background-color: #222;
+}
+
+input:focus+.slider {
+  box-shadow: 0 0 1px #222;
+}
+
+input:checked+.slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+.button1 {
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.1);
+  transition-duration: 300ms;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: 0.6s ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  translate: 0px 150px;
+}
+</style>
--- src/pages/notfound.vue
+++ src/pages/notfound.vue
@@ -3,14 +3,19 @@
 
 </script>
 <template>
-    <div class="flex justify-center">
-        <img class="w-12  mb-3 animate-pulse mt-12" src="../assets/images/error.png" alt="">
+    <div class="dark:bg-dark-black">
+        <div class="flex justify-center ">
+            <img class="w-12  mb-3 animate-pulse mt-12" src="../assets/images/error.png" alt="">
+        </div>
+        <h1
+            class="text-50s font-reemkufi font-semibold tracking-widest dark:text-white text-dark-black text-center mt-4 ">
+            PAGE</h1>
     </div>
-    <h1 class="text-50s font-reemkufi font-semibold tracking-widest text-dark-black text-center mt-4 ">PAGE</h1>
-    <div class="bg-bg-grey">
-        <h1 class="text-50s font-reemkufi font-semibold tracking-widest text-dark-black text-center">NOT FOUND
+    <div class="bg-bg-grey dark:bg-dark-black">
+        <h1 class="text-50s font-reemkufi font-semibold tracking-widest dark:text-white text-dark-black text-center">NOT
+            FOUND
         </h1>
-        <div class="flex justify-center">
+        <div class="flex justify-center dark:text-white">
             <div class="flex-col  text-center font-ralewayp-5  m-6 rounded-2xl ">
                 <h2>Welp..<br>Looks like
                     you got lost again !<span class="block font-semibold">Click on the button to get to the home
@@ -20,6 +25,7 @@
                     <router-link to="/"> HOME PAGE
                     </router-link>
                 </div>
+
 
             </div>
         </div>
